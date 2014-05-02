f03c3778c1abf8dcb05165f6e38979197ab8a67a
Danail Koychev <danail.k.koychev@gmail.com>
2014-05-02 20:50:17 +0300

    add damage to projectile, remove effects list

--- projectile.py
+++ projectile.py
@@ -6,23 +6,35 @@ class Projectile(Sprite):
         Sprite.__init__(self)
         self.position = position
         self.image = image
+        self.rect = image.get_rect()
+        self.rect.x = position.x
+        self.rect.y = position.y
         self.speed = speed
         self.aim = aim
         self.effects = effects
         self.movement_vector = self.get_movement_vector()
-        self.rect = image.get_rect()
-        self.rect.x = position.x
-        self.rect.y = position.y
 
     def get_movement_vector(self):
-        movement_vector = Vector(self.aim.x - self.position.x,
-                                 self.aim.y - self.position.y)
-        movement_vector.normalize()
+        movement_vector = Vector(self.aim.x - self.rect.x,
+                                 self.aim.y - self.rect.y)
+        movement_vector = movement_vector.normalize()
         return movement_vector
 
-    def move(self, time_passed):
-        self.rect.x += self.movement_vector.x * self.speed * time_passed
-        self.rect.y += self.movement_vector.y * self.speed * time_passed
+    def update(self, time_passed):
+        self.position.x += self.movement_vector.x * self.speed * time_passed
+        self.position.y += self.movement_vector.y * self.speed * time_passed
+        self.rect.x = self.position.x
+        self.rect.y = self.position.y
 
-    def reflect_vertical(self):
-        
+    def reflect_horizontally(self, x_position):
+        self.aim.x = 2 * x_position - self.aim.x
+        self.movement_vector = self.get_movement_vector()
+
+    # TODO: position aim on the other side of the screen
+    # def reflect_vertically(self, y_position):
+    #     self.aim.y = 405
+    #     self.movement_vector = self.get_movement_vector()
+
+
+
+
