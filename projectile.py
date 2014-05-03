46e0e327e7cef1afbc97777c2a25dbcd85ac72cf
Danail Koychev <danail.k.koychev@gmail.com>
2014-05-03 20:08:56 +0300

    add 'position' property to also change 'rect'

--- projectile.py
+++ projectile.py
@@ -2,38 +2,40 @@ from pygame.sprite import Sprite, Group, Rect
 from pygame.math import Vector2 as Vector
 
 class Projectile(Sprite):
-    def __init__(self, image, position, speed, aim, effects=[]):
+    def __init__(self, image, position, speed, aim, damage):
         Sprite.__init__(self)
+        self.rect = image.get_rect()
         self.position = position
         self.image = image
-        self.rect = image.get_rect()
-        self.rect.x = position.x
-        self.rect.y = position.y
         self.speed = speed
         self.aim = aim
-        self.effects = effects
+        self.damage = damage
         self.movement_vector = self.get_movement_vector()
 
+    @property
+    def position(self):
+        return self._position
+    @position.setter
+    def position(self, value):
+        self.rect.x = value.x
+        self.rect.y = value.y
+        self._position = value
+
     def get_movement_vector(self):
-        movement_vector = Vector(self.aim.x - self.rect.x,
-                                 self.aim.y - self.rect.y)
+        movement_vector = Vector(self.aim.x - self.position.x,
+                                 self.aim.y - self.position.y)
         movement_vector = movement_vector.normalize()
         return movement_vector
 
     def update(self, time_passed):
-        self.position.x += self.movement_vector.x * self.speed * time_passed
-        self.position.y += self.movement_vector.y * self.speed * time_passed
-        self.rect.x = self.position.x
-        self.rect.y = self.position.y
+        self.position += self.movement_vector * self.speed * time_passed 
 
     def reflect_horizontally(self, x_position):
         self.aim.x = 2 * x_position - self.aim.x
         self.movement_vector = self.get_movement_vector()
 
-    # TODO: position aim on the other side of the screen
     # def reflect_vertically(self, y_position):
-    #     self.aim.y = 405
-    #     self.movement_vector = self.get_movement_vector()
+    #
 
 
 
