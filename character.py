4b92d7ca475b9f17d5b77e7804c8ffb59c59af26
Danail Koychev <danail.k.koychev@gmail.com>
2014-05-14 18:39:32 +0300

    seperate gui from core

--- character.py
+++ character.py
@@ -3,19 +3,16 @@ from copy import copy
 from point import *
 from projectile import *
 
-class Character(Sprite):
-    def __init__(self, image, position, move_speed, reload_time_millisec,
+class Character():
+    def __init__(self, position, move_speed, reload_time_millisec,
                  projectile_type, health, damage, aim_speed):
-        Sprite.__init__(self)
-        self.rect = image.get_rect()
         self.position = position
-        self.image = image
         self.move_speed = move_speed
         self.reload_time_millisec = reload_time_millisec
         self.projectile_type = projectile_type
         self.health = health
         self.damage = damage
-        self.active_projectiles = Group()
+        self.active_projectiles = []
         self.aim = Point(400, 0)
         self.aim_speed = aim_speed
 
@@ -24,27 +21,16 @@ class Character(Sprite):
         self.snared = False
         self.vampire = False
 
-    @property
-    def position(self):
-        return self._position
-    @position.setter
-    def position(self, value):
-        self.rect.x = value.x
-        self.rect.y = value.y
-        self._position = value
-
     def move_left(self, time_passed):
         if not self.snared:
             distance_traveled = self.move_speed * time_passed
             self.position.x -= distance_traveled
-            self.rect.x = self.position.x
             self.aim.x -= distance_traveled # alternative aiming system
 
     def move_righ(self, time_passed):
         if not self.snared:
             distance_traveled = self.move_speed * time_passed
             self.position.x += distance_traveled
-            self.rect.x = self.position.x
             self.aim.x += distance_traveled # alternative aiming system
  
     def try_shoot(self, time_passed):
@@ -60,7 +46,7 @@ class Character(Sprite):
                           #self.aim,
                           0)
         shot.movement_vector = shot.get_movement_vector(self.aim)
-        self.active_projectiles.add(shot)
+        self.active_projectiles.append(shot)
         return shot
 
     def start_reloading(self):
--- projectile.py
+++ projectile.py
@@ -1,23 +1,12 @@
 from pygame.sprite import Sprite, Group, Rect
 from pygame.math import Vector2 as Vector
 
-class Projectile(Sprite):
-    def __init__(self, image, position, speed, damage):
-        Sprite.__init__(self)
-        self.rect = image.get_rect()
+class Projectile():
+    def __init__(self, position, speed, damage, master=None):
         self.position = position
-        self.image = image
         self.speed = speed
         self.damage = damage
-
-    @property
-    def position(self):
-        return self._position
-    @position.setter
-    def position(self, value):
-        self.rect.x = value.x
-        self.rect.y = value.y
-        self._position = value
+        self.master = master
 
     def get_movement_vector(self, aim):
         movement_vector = Vector(aim.x - self.position.x,
@@ -26,8 +15,6 @@ class Projectile(Sprite):
         return movement_vector
 
     def update(self, time_passed):
-        #
-        #self.movement_vector = self.get_movement_vector()
         self.position += self.movement_vector * self.speed * time_passed 
 
     def reflect_horizontally(self, x_position):
@@ -37,22 +24,21 @@ class Projectile(Sprite):
     def reflect_vertically(self, field_height):
         self.movement_vector.y = -self.movement_vector.y
 
-# FINISH 
+
 # use instance of
-    def hit(self, obstacle):
-        if obstacle.__class__ is Character:
-            character.health -= self.damage
-            self.die()
-        if obstacle.__class__ is Wall:
-            if obstacle.power_up is not None:
-                pass
+    # def hit(self, obstacle):
+    #     if obstacle.__class__ is Character:
+    #         character.health -= self.damage
+    #         self.die()
+    #     if obstacle.__class__ is Wall:
+    #         if obstacle.power_up is not None:
+    #             pass
                 #apply
             # self.reflect_vertically(self,position.y)
 
-# remove group from pygame sprite?
-    def die(self):
-        for group in self.groups():
-            group.remove(self)
+# remove group
+    # def die(self):
+    #     self.master.active_projectiles.remove(self)
 
 
 # verically only
