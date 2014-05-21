5f315d3b713b9817ee036b4cada60d67b0d2731b
Danail Koychev <danail.k.koychev@gmail.com>
2014-05-21 17:16:48 +0300

    add 'state' attribute

--- character.py
+++ character.py
@@ -3,10 +3,18 @@ from copy import copy
 from point import *
 from projectile import *
 
+MOVING_LEFT = 0
+STATIONARY = 1
+MOVING_RIGHT = 2
+DEAD = 3
+
+
 class Character():
-    def __init__(self, position, move_speed, reload_time_millisec,
+    def __init__(self, position, size, move_speed, reload_time_millisec,
                  projectile_type, health, damage, aim_speed):
+        self.state = STATIONARY
         self.position = position
+        self.size = size
         self.move_speed = move_speed
         self.reload_time_millisec = reload_time_millisec
         self.projectile_type = projectile_type
@@ -27,7 +35,7 @@ class Character():
             self.position.x -= distance_traveled
             self.aim.x -= distance_traveled # alternative aiming system
 
-    def move_righ(self, time_passed):
+    def move_right(self, time_passed):
         if not self.snared:
             distance_traveled = self.move_speed * time_passed
             self.position.x += distance_traveled
@@ -39,11 +47,10 @@ class Character():
             self.start_reloading()
 
     def shoot(self):
-        shot = Projectile(self.projectile_type.image,
-                          Point(self.position.x + self.rect.width/2,
+        shot = Projectile(Point(self.position.x + self.size.y/2,
                                 self.position.y),
+                          self.projectile_type.size,
                           self.projectile_type.speed,
-                          #self.aim,
                           0)
         shot.movement_vector = shot.get_movement_vector(self.aim)
         self.active_projectiles.append(shot)
@@ -60,6 +67,11 @@ class Character():
         self.aim.x += self.aim_speed * time_passed
 
     def update(self, time_passed):
+        if self.state == MOVING_LEFT:
+            self.move_left(time_passed)
+        if self.state == MOVING_RIGHT:
+            self.move_right(time_passed)
+
         if self.disarmed:
             self.disarm_time_left -= time_passed
             if self.disarm_time_left <= 0:
