57af3d59d34e529885540aa35f8bafdf21f18de4
Danail Koychev <danail.k.koychev@gmail.com>
2014-05-29 01:31:15 +0300

    add 'stay_hidden()' and functions it uses

--- bot.py
+++ bot.py
@@ -4,10 +4,17 @@ import character
 import random
 import math
 
+from game import *
+import character
+
 class Bot:
     def __init__(self, character, game):
         self.character = character
         self.game = game
+
+        self.hidden = False
+        self.wall_hiding_self = None
+
         if self.game.player_1 == character:
             self.enemy = self.game.player_2
         else:
@@ -16,14 +23,14 @@ class Bot:
     # def spray():
     #     pass
 
-    def get_approximate_projectile_fight_time(self):
+    def get_approximate_own_projectile_flight_time(self):
         self.character.projectile_type.position = self.character.position
         return math.fabs(self.character.position.y - self.enemy.position.y) / \
-               math.fabs(self.player_1.projectile_type.get_movement_vector(self.player_1.aim).y)
+               math.fabs(self.character.projectile_type.get_movement_vector(self.enemy.position ).y)
 
-    def get_enemy_movement_range(self, time): 
-        return (self.enemy.position.x - self.enemy.movement_speed * time, 
-                self.enemy.position.x + self.enemy.movement_speed * time)
+    def get_movement_range(self, character, time): 
+        return (character.position.x - character.movement_speed * time, 
+                character.position.x + character.movement_speed * time)
 
     def predict_enemy_position(self, time):
         expected_position_x = 0
@@ -36,8 +43,49 @@ class Bot:
         else:
             expected_position_x = self.enemy_position.x
 
-        if expected_position_x < 0:
-            expected_position_x = 0
-        elif expected_position_x > self.game.field_width:
-            expected_position_x = self.game.field_width
+        if expected_position_x < self.enemy.size.x / 2:
+            expected_position_x = self.enemy.size.x / 2
+        elif expected_position_x > self.game.field_width - self.enemy.size.x / 2:
+            expected_position_x = self.game.field_width - self.enemy.size.x / 2
         return expected_position_x
+
+    def move_towards(self, point_x):
+        if self.character.position.x > point_x:
+            self.character.state = MOVING_LEFT
+        elif self.character.position.x < point_x:
+            self.character.state = MOVING_RIGHT
+        else:
+            self.character.state = STATIONARY
+
+    def is_hidden(self):
+        middle_point = self.character.position.x + self.enemy.position
+        for wall in self.game.walls:
+            if math.fabs(wall.position.x - middle_point) < WALL_LENGTH:
+                return True
+        return False
+
+    def stay_hidden(self):
+        safest_point = 2 * self.wall_hiding_self.position.x - self.enemy.position.x
+        if math.fabs(self.character.position.x - safest_point) > \
+           (self.character.speed * 1000) / Game.FPS:
+            self.move_towards(safest_point)
+        else:
+            self.character.position.x = safest_point
+        #if safest_point > 0 and safest_point < Game.FIELD_WIDTH:
+
+#    def unhide(self):
+ 
+    def get_closest_wall_to_reach(self):    # if the current closest wall is moving away it might not be the fastest one to reach
+        closest_wall = None
+        distance = Game.FIELD_WIDTH
+        for wall in self.game.walls:
+            if math.fabs(wall.position.x - self.character.position.x) < distance:
+                distance = math.fabs(wall.position.x - self.character.position.x)
+                closest_wall = wall
+        return closest_wall
+
+    # def move_away(self, projectile):
+    #     if projectile.
+
+
+
