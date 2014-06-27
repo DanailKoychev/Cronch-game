9e2c61cd77a0ce2dc88b0aa8eb5937c533f10ddc
Danail Koychev <danail.k.koychev@gmail.com>
2014-06-27 19:53:32 +0300

    add a 'tank'character class

---
+++ tank.py
@@ -0,0 +1,49 @@
+import character
+import projectile
+import point
+
+TANK_SIZE = point.Point(80, 80)
+TANK_SPEED = 0.3
+TANK_RELOAD_TIME = 300
+TANK_PROJECTILE_TYPE = projectile.Projectile(point.Point(50, 50),
+                                             point.Point(10, 10),
+                                             1, 1, point.Point(0, 0))
+TANK_HEALTH = 350
+TANK_DAMAGE = 1
+
+MISSLE_DAMAGE = 15
+MISSLE_HEALTH_COST = 15
+
+class Tank(character.Character):
+
+    def __init__(self, position):
+        character.Character.__init__(self, position, TANK_SIZE,
+            TANK_SPEED, TANK_RELOAD_TIME, TANK_PROJECTILE_TYPE,
+            TANK_HEALTH, TANK_DAMAGE)
+        self.aggressive_mode = False
+
+    def use_skill(self):
+        if self.aggressive_mode == False:
+            self.damage = MISSLE_DAMAGE
+            self.health -= MISSLE_HEALTH_COST
+            self.projectile_type.size.x *= 3.5
+            self.projectile_type.size.y *= 3.5
+            self.aggressive_mode = True
+
+    def get_shot(self):
+        shot = character.Character.get_shot(self)
+        if self.aggressive_mode == True:
+            self.damage = TANK_DAMAGE
+            self.aggressive_mode = False
+            self.projectile_type.size.x /= 3.5
+            self.projectile_type.size.y /= 3.5
+        return shot
+
+
+
+
+        # shot = self.try_shoot()
+        # self.damage = TANK_DAMAGE
+        # self.projectile_type.size.x /= 3.5
+        # self.projectile_type.size.y /= 3.5
+        # return shot
