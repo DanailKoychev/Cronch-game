5791cb16744f386f800c76bda8a88e0e878d8e4c
Danail Koychev <danail.k.koychev@gmail.com>
2014-06-28 12:41:47 +0300

    balance change

--- tank_bot.py
+++ tank_bot.py
@@ -4,7 +4,7 @@ import controls
 class TankBot(bot.Bot):
     def get_input(self):
         instructions = bot.Bot.get_input(self)
-        if not self.defensive:
-            if self.character.health > 1.5 * self.enemy.health:
-                instructions.append(controls.Controls.USE_SKILL)
+        if not self.defensive and \
+           self.character.health > 1.5 * self.enemy.health:
+            instructions.append(controls.Controls.USE_SKILL)
         return instructions
