8d967ac94223b07182bf2251925056da6ae62d37
Danail Koychev <danail.k.koychev@gmail.com>
2014-06-28 12:42:32 +0300

    add bot tests to the rest

--- test_all.py
+++ test_all.py
@@ -4,6 +4,7 @@ from test_game import *
 from test_wall import *
 from test_projectile import *
 from test_character import *
+from test_bot import *
 
 if __name__ == '__main__':
     unittest.main()
