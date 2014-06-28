d325b238ca8adaffcbdd47db3209ef13da1d7ade
Danail Koychev <danail.k.koychev@gmail.com>
2014-06-28 13:51:38 +0300

    Update wall.py

--- wall.py
+++ wall.py
@@ -2,21 +2,20 @@ import random
 
 from point import *
 
-HEAL = 0
-SNARE = 1
-VAMIPIRE = 2
-SPEED_UP = 3
-DISARM = 4
-
-VAMPIRE_PERCENT = 60
-VAMPIRE_TIME = 10000
-SPEED_UP_PERCENT = 20
-HEAL_VALUE = 30
-DISARM_TIME_MILLISEC = 4000
-SNARE_TIME_MILLISEC = 2000
-DAMAGE_UP_PERCENT = 30
-
 class Wall():
+    HEAL = 0
+    SNARE = 1
+    VAMIPIRE = 2
+    SPEED_UP = 3
+    DISARM = 4
+    
+    VAMPIRE_PERCENT = 60
+    VAMPIRE_TIME = 10000
+    SPEED_UP_PERCENT = 20
+    HEAL_VALUE = 30
+    DISARM_TIME_MILLISEC = 4000
+    SNARE_TIME_MILLISEC = 2000
+    DAMAGE_UP_PERCENT = 30
 
     SPEED = 0.09
 
@@ -44,34 +43,6 @@ class Wall():
             self.move_right(time_passed)
         else:
             self.move_left(time_passed)
-
-
-
-    # def get_power_up(self, power_up=None):
-    #     if power_up is not None:
-    #         self.power_up = power_up
-    #     else:
-    #         power_up = POWER_UPS[random.randint(0, len(POWER_UPS))]
-    
-    # def give_speed_up(self, character):
-    #     character.speed *= 1 + SPEED_UP_PERCENT / 100
-
-    # def give_bonus_hp(self, character):
-    #     character.health += HEAL_VALUE
-
-    # def give_vampire(self, character):
-    #     character.vampire = VAMPIRE_PERCENT / 100
-
-    # def give_disarm(self, character):
-    #     character.disarmed = True
-    #     character.disarmed_time = DISARM_TIME_MILLISEC
-
-    # def give_snare(self, character):
-    #     character.snared = True
-    #     character.snare_time_left = SNARE_TIME_MILLISEC
-
-    # def give_gamage(self, character):
-    #     character.damage *= 1 + DAMAGE_UP_PERCENT / 100
 
     @property
     def x(self):
