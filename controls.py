b2d0806ca79dd6f68060146f3bd8a2ef1a5370b2
Danail Koychev <danail.k.koychev@gmail.com>
2014-06-28 08:41:28 +0300

    add player 2 controls

--- controls.py
+++ controls.py
@@ -1,32 +1,44 @@
 from pygame.locals import *
 import pygame
 
-from projectile import *
-from character import *
-from point import *
-from wall import *
+class Controls:
+    MOVE_LEFT = 1
+    MOVE_RIGHT = 2
+    USE_SKILL = 3
+    AIM_LEFT = 4
+    AIM_RIGHT = 5
+    SHOOT = 6
 
-MOVE_LEFT = 1
-MOVE_RIGHT = 2
-USE_SKILL = 3
-AIM_LEFT = 4
-AIM_RIGHT = 5
-SHOOT = 6
+    def get_keyboard_input_player_1():
+        instructions = []
+        keys = pygame.key.get_pressed()
+        if keys[K_KP1]:
+            instructions.append(Controls.MOVE_LEFT)
+        if keys[K_KP3]:
+            instructions.append(Controls.MOVE_RIGHT)
+        if keys[K_KP5]:
+            instructions.append(Controls.USE_SKILL)
+        if keys[K_LEFT]:
+            instructions.append(Controls.AIM_LEFT)
+        if keys[K_RIGHT]:
+            instructions.append(Controls.AIM_RIGHT)
+        if keys[K_UP]:
+            instructions.append(Controls.SHOOT)
+        return instructions
 
-def get_keyboard_input_player_1():
-    instructions = []
-    keys = pygame.key.get_pressed()
-    if keys[K_LEFT]:
-        instructions.append(MOVE_LEFT)
-    if keys[K_RIGHT]:
-        instructions.append(MOVE_RIGHT)
-    if keys[K_UP]:
-        instructions.append(USE_SKILL)
-    if keys[K_a]:
-        instructions.append(AIM_LEFT)
-    if keys[K_d]:
-        instructions.append(AIM_RIGHT)
-    if keys[K_w]:
-        instructions.append(SHOOT)
-    return instructions
- 
+    def get_keyboard_input_player_2():
+        instructions = []
+        keys = pygame.key.get_pressed()
+        if keys[K_g]:
+            instructions.append(Controls.MOVE_LEFT)
+        if keys[K_j]:
+            instructions.append(Controls.MOVE_RIGHT)
+        if keys[K_y]:
+            instructions.append(Controls.USE_SKILL)
+        if keys[K_a]:
+            instructions.append(Controls.AIM_LEFT)
+        if keys[K_d]:
+            instructions.append(Controls.AIM_RIGHT)
+        if keys[K_w]:
+            instructions.append(Controls.SHOOT)
+        return instructions 
